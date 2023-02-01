2cb28a3fa0483c346d8ee2ca00390382b2442689
ozkanonur <work@onurozkan.dev>
2023-02-02 01:50:03 +0300

    create qb script for killing youtube ads manually
    Signed-off-by: ozkanonur <work@onurozkan.dev>

---
+++ .config/qutebrowser/greasemonkey/youtube_ad_blocker.js
@@ -0,0 +1,10 @@
+setInterval(() => {
+    const btn = document.querySelector('.videoAdUiSkipButton,.ytp-ad-skip-button')
+    if (btn) {
+        btn.click()
+    }
+    const ad = [...document.querySelectorAll('.ad-showing')][0];
+    if (ad) {
+        document.querySelector('video').playbackRate = 16;
+    }
+}, 1)
