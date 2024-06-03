ef5c7b1e5712be1b8d90e15e76a7fbeaefc932a4
onur-ozkan <work@onurozkan.dev>
2024-06-03 08:00:17 +0300

    disable LSP window borders
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- .config/nvim/lua/cfg_lsp.lua
+++ .config/nvim/lua/cfg_lsp.lua
@@ -56,27 +56,27 @@ vim.fn.sign_define("LspDiagnosticsSignHint", {
 })
 
 -- LSP Borders
-local _border = "single"
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-  vim.lsp.handlers.hover, {
-    border = _border
-  }
-)
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-  vim.lsp.handlers.signature_help, {
-    border = _border
-  }
-)
-
-vim.diagnostic.config{
-  float={border=_border}
-}
-
-require('lspconfig.ui.windows').default_options = {
-  border = _border
-}
+-- local _border = "single"
+-- 
+-- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+--   vim.lsp.handlers.hover, {
+--     border = _border
+--   }
+-- )
+-- 
+-- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+--   vim.lsp.handlers.signature_help, {
+--     border = _border
+--   }
+-- )
+-- 
+-- vim.diagnostic.config{
+--   float={border=_border}
+-- }
+-- 
+-- require('lspconfig.ui.windows').default_options = {
+--   border = _border
+-- }
 -- LSP Borders
 
 -- Diagnostic prefix
